05187183b91f77ed3e275c6b329ed916df44bf6a
陈善文 <obaba@eore.cn>
2021-08-03 08:48:04 +0800

    使用 Universal Redis client 统一集群、主从、哨兵模式的连接

--- library/sredis/sredis.go
+++ library/sredis/sredis.go
@@ -10,54 +10,49 @@ import (
 )
 
 const (
-	// Default group name for instance usage.
+	// DEFAULT_NAME Default group name for instance usage.
 	DEFAULT_NAME     = "default"
 	gREDIS_NODE_NAME = "redis"
 )
 
 var (
 	// Instances map containing configuration instances.
-	instances   = gmap.NewStrAnyMap(true)
-	clusterOpts *redis.ClusterOptions
-	opts        *redis.Options
+	instances = gmap.NewStrAnyMap(true)
 )
 
-//ClusterClient 返回一个 go-redis 的集群单例
-func ClusterClient(name ...string) *redis.ClusterClient {
+//Client 返回一个 go-redis 的单例
+//UniversalClient is an abstract client which - based on the provided options - represents either a ClusterClient, a FailoverClient, or a single-node Client. This can be useful for testing cluster-specific applications locally or having different clients in different environments.
+//
+//NewUniversalClient returns a new multi client. The type of the returned client depends on the following conditions:
+//
+//If the MasterName option is specified, a sentinel-backed FailoverClient is returned.
+//if the number of Addrs is two or more, a ClusterClient is returned.
+//Otherwise, a single-node Client is returned.
+//For example:
+//
+//
+//// rdb is *redis.Client.
+//rdb := NewUniversalClient(&redis.UniversalOptions{
+//    Addrs: []string{":6379"},
+//})
+//
+//// rdb is *redis.ClusterClient.
+//rdb := NewUniversalClient(&redis.UniversalOptions{
+//    Addrs: []string{":6379", ":6380"},
+//})
+//
+//// rdb is *redis.FailoverClient.
+//rdb := NewUniversalClient(&redis.UniversalOptions{
+//    Addrs: []string{":6379"},
+//    MasterName: "mymaster",
+//})
+func Client(name ...string) redis.UniversalClient {
 	config := gins.Config()
 	key := DEFAULT_NAME
 	if len(name) > 0 && name[0] != "" {
 		key = name[0]
 	}
-	return instances.GetOrSetFuncLock(key, func() interface{} {
-		var m map[string]interface{}
-		if _, v := gutil.MapPossibleItemByKey(gins.Config().GetMap("."), gREDIS_NODE_NAME); v != nil {
-			m = gconv.Map(v)
-		}
-		if len(m) > 0 {
-			if v, ok := m[key]; ok {
-				err := gconv.Struct(v, &clusterOpts)
-				if err != nil {
-					panic(err)
-				}
-				return redis.NewClusterClient(clusterOpts)
-			} else {
-				panic(fmt.Sprintf(`configuration for redis not found for group "%s"`, key))
-			}
-		} else {
-			panic(fmt.Sprintf(`incomplete configuration for redis: "redis" node not found in config file "%s"`, config.GetFileName()))
-		}
-		return nil
-	}).(*redis.ClusterClient)
-}
-
-//Client 返回一个 go-redis 的集群单例
-func Client(name ...string) *redis.Client {
-	config := gins.Config()
-	key := DEFAULT_NAME
-	if len(name) > 0 && name[0] != "" {
-		key = name[0]
-	}
+	var opts *redis.UniversalOptions
 	return instances.GetOrSetFuncLock(key, func() interface{} {
 		var m map[string]interface{}
 		if _, v := gutil.MapPossibleItemByKey(gins.Config().GetMap("."), gREDIS_NODE_NAME); v != nil {
@@ -69,7 +64,7 @@ func Client(name ...string) *redis.Client {
 				if err != nil {
 					panic(err)
 				}
-				return redis.NewClient(opts)
+				return redis.NewUniversalClient(opts)
 			} else {
 				panic(fmt.Sprintf(`configuration for redis not found for group "%s"`, key))
 			}
@@ -77,5 +72,5 @@ func Client(name ...string) *redis.Client {
 			panic(fmt.Sprintf(`incomplete configuration for redis: "redis" node not found in config file "%s"`, config.GetFileName()))
 		}
 		return nil
-	}).(*redis.Client)
+	}).(redis.UniversalClient)
 }
